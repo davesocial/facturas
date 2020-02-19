645c28437bfde5c18598ff255c7cee99d4311139
David Cedeño <davidcedeno@MacBook-Pro-de-David.local>
2020-02-19 13:54:20 +0100

    versionfinprod

--- src/main/java/com/once/facturas/controller/ProductoController.java
+++ src/main/java/com/once/facturas/controller/ProductoController.java
@@ -1,20 +1,27 @@
 package com.once.facturas.controller;
 
+import com.once.facturas.model.Producto;
 import com.once.facturas.model.ProductoRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * ProductoController
  */
-@Controller
+@RestController
+@RequestMapping (value = "/productos")
 public class ProductoController {
 
     @Autowired
     ProductoRepository productoRepository;
+
+    @GetMapping ("/")
+    public Iterable<Producto> getAllProductoS(){
+        return productoRepository.findAll();
+    }
 
     @RequestMapping("/producto")
     @ResponseBody
