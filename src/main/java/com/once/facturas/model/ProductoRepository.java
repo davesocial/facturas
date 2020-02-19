b94b44d1fc83a3e1e8d04b5f629ec1dbd1d65fc5
David Cedeño <davidcedeno@MacBook-Pro-de-David.local>
2020-02-19 11:35:29 +0100

    arriba5

--- src/main/java/com/once/facturas/model/ProductoRepository.java
+++ src/main/java/com/once/facturas/model/ProductoRepository.java
@@ -2,7 +2,7 @@ package com.once.facturas.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoRepository extends CrudRepository<Producto, Integer>{
+public interface ProductoRepository extends CrudRepository<Producto, Long>{
 
 
 }
