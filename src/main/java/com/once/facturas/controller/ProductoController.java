1213934a391e2d97365a916b57a2f02328891b62
David Cedeño <davidcedeno@MacBook-Pro-de-David.local>
2020-02-19 12:15:42 +0100

    finalproducto

--- src/main/java/com/once/facturas/controller/ProductoController.java
+++ src/main/java/com/once/facturas/controller/ProductoController.java
@@ -1,5 +1,8 @@
 package com.once.facturas.controller;
 
+import com.once.facturas.model.ProductoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,10 +13,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ProductoController {
 
+    @Autowired
+    ProductoRepository productoRepository;
+
     @RequestMapping("/producto")
     @ResponseBody
     public String producto(){
         return "Hola producto";
     }
     
+    @RequestMapping("/count")
+    @ResponseBody
+    public String count(){
+        return "Tengo" + String.valueOf(productoRepository.count()) + "productos.";
+    }
+
 }
