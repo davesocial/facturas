9650e6ef6716d888777c0a434f7e6f849e16d46d
David Cedeño <davidcedeno@MacBook-Pro-de-David.local>
2020-02-20 12:39:18 +0100

    versionfinpro20

--- src/main/java/com/once/facturas/controller/ProductoController.java
+++ src/main/java/com/once/facturas/controller/ProductoController.java
@@ -1,9 +1,14 @@
 package com.once.facturas.controller;
 
+import java.util.NoSuchElementException;
+
 import com.once.facturas.model.Producto;
 import com.once.facturas.model.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +26,26 @@ public class ProductoController {
     @GetMapping ("/")
     public Iterable<Producto> getAllProductoS(){
         return productoRepository.findAll();
+    }
+
+    @GetMapping("{id}/")
+    public Producto getProducto(
+            @PathVariable("id") Long id
+        ){
+            
+         try {
+            return productoRepository.findById(id).get(); 
+         } catch (NoSuchElementException e) {
+             return null;
+         }
+    }
+
+    @PostMapping ("/")
+    public Producto creaProducto(
+        @RequestBody Producto producto
+    ) {
+        Producto p = productoRepository.save(producto);
+        return p;
     }
 
     @RequestMapping("/producto")
